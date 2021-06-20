353bbb0fbb8dc8f76eb03c7f3dd89359b7c3d8d9
Ashish oli <oliashish3@gmail.com>
2021-06-21 01:57:21 +0530

    backend setup completed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { v4: uuidV4 } = require("uuid");
 const cors = require("cors");
 const socket = require("socket.io");
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +13,22 @@ const io = socket(server);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+io.on("connection", (socket) => {
+    socket.on("self", socket.id);
+
+    socket.on("call", ({ user, signalData, from, username }) => {
+        io.to(user).emit("call", { signal: signalData, from, username });
+    });
+
+    socket.on("answer", (data) => {
+        io.on(data.to).emit("answer", data.signal);
+    });
+
+    socket.on("end", () => {
+        socket.broadcast.emit("callEnded");
+    });
+});
 
 server.listen(PORT, () => {
     console.log("listening to port : ", PORT);
