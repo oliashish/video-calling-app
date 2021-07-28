6b7d6184fcd784eabd1aa54ec39885d8deebcd27
oliashish <oliashish3@gmail.com>
2021-07-28 13:17:09 +0530

    alignment and cors issues done

--- client/src/App.js
+++ client/src/App.js
@@ -107,50 +107,59 @@ function App() {
                 />
 
                 <div className="myId">
-                    <TextField
-                        id="filled-basic"
-                        label="Name"
-                        variant="filled"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        style={{ marginBottom: "30px" }}
-                    />
-                    <CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            startIcon={<AssignmentIcon fontSize="large" />}
+                    <h3>
+                        Copy your Id and send it to the person who you want to
+                        call
+                    </h3>
+                    <div className="info-options">
+                        <TextField
+                            id="filled-basic"
+                            label="Name"
+                            variant="filled"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            style={{ marginBottom: "30px" }}
+                        />
+                        <CopyToClipboard
+                            text={me}
+                            style={{ marginBottom: "2rem" }}
                         >
-                            Copy ID
-                        </Button>
-                    </CopyToClipboard>
-
-                    <TextField
-                        id="filled-basic"
-                        label="ID to call"
-                        variant="filled"
-                        value={idToCall}
-                        onChange={(e) => setIdToCall(e.target.value)}
-                    />
-                    <div className="call-button">
-                        {callAccepted && !callEnded ? (
                             <Button
                                 variant="contained"
-                                color="secondary"
-                                onClick={leaveCall}
-                            >
-                                End Call
-                            </Button>
-                        ) : (
-                            <IconButton
                                 color="primary"
-                                aria-label="call"
-                                onClick={() => callUser(idToCall)}
+                                startIcon={<AssignmentIcon fontSize="large" />}
                             >
-                                <PhoneIcon fontSize="large" />
-                            </IconButton>
-                        )}
-                        {idToCall}
+                                Copy ID
+                            </Button>
+                        </CopyToClipboard>
+
+                        <TextField
+                            id="filled-basic"
+                            label="ID to call"
+                            variant="filled"
+                            value={idToCall}
+                            onChange={(e) => setIdToCall(e.target.value)}
+                        />
+                        <div className="call-button">
+                            {callAccepted && !callEnded ? (
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={leaveCall}
+                                >
+                                    End Call
+                                </Button>
+                            ) : (
+                                <IconButton
+                                    color="primary"
+                                    aria-label="call"
+                                    onClick={() => callUser(idToCall)}
+                                >
+                                    <PhoneIcon fontSize="large" />
+                                </IconButton>
+                            )}
+                            {idToCall}
+                        </div>
                     </div>
                 </div>
                 <div>
