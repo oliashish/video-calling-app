ebe452bd502100ad346529752ddccaddf2be43ec
oliashish <oliashish3@gmail.com>
2021-07-28 22:14:22 +0530

    modified server.js and package.json for deployment

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ io.on("connection", (socket) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    server.use(express.static("./client/build"));
+    app.use(express.static("./client/build"));
 }
-server.get("/*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-server.listen(PORT, () => console.log("server is running on port", PORT));
+app.listen(PORT, () => console.log("server is running on port", PORT));
